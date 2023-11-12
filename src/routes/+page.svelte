b2a01734ce1f8c9b7a5067df24127c6d6df181a5
vionicbest <38846024+vionicbest@users.noreply.github.com>
2023-11-13 08:27:14 +0900

    첫번째 페이지 구성 일부 완료

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,156 +1,140 @@
 <script lang="ts">
-  import { PdfViewer } from "svelte-pdf-simple";
-  import type { PdfPageContent, PdfLoadSuccess, PdfException } from "svelte-pdf-simple";
-    import type { EventHandler, MouseEventHandler } from "svelte/elements";
-
-  let pdfViewer: PdfViewer;
-  let pageNumber = 0;
-  let totalPages = 0;
-  let isPdfLoaded = false;
-  let scale = 1.5;
-  let divElement: HTMLDivElement;
-  const pdfPath = "/files/sample.pdf"
-
-  function handlePageChanged(event: CustomEvent<PdfPageContent>): void {
-    pageNumber = event.detail.pageNumber;
-  }
-  function goToPage(page: number): void {
-    pdfViewer.goToPage(page);
-  }
-  function handleLoadedSuccess(event: CustomEvent<PdfLoadSuccess>) {
-    console.info("loaded", event.detail);
-    totalPages = event.detail.totalPages;
-    pageNumber = 1;
-    isPdfLoaded = true;
-  }
-  function handleLoadFailure(event: CustomEvent<PdfException>) {
-    console.info("failed", event.detail);
-  }
-  
-  function handleClick(event: MouseEvent ): void {
-    const clickX = event.clientX - divElement.getBoundingClientRect().left;
-    const divWidth = divElement.clientWidth;
-
-    if (clickX < divWidth / 2) {
-      goToPage(pageNumber - 1);
-    } else {
-      goToPage(pageNumber + 1);
-    }
-  }
-  
-  function handleDownloadClick() {
-    const fileData = "";
-    const fileName = pdfPath;
-
-    const blob = new Blob([fileData], { type: "text/plain"});
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = fileName;
-    document.body.appendChild(a);
-    a.click();
-
-    URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  }
-
 </script>
 
-<div class='title'>
-  <div class='logo'>
-    <h1>SNU Computing Commons</h1>
-    <h2>건축 기금 모금</h2>
-  </div>
-  <div class='symbol'>
-    <img src='/images/snu_symbol.png' alt='서울대학교 로고'/>
-  </div>
-</div>
-<div class='container'>
-  <div class='pamphlet_download'>
-    <img 
-      class='pdf_download'
-      src="/images/pdf_download.png"
-      alt="SNU Computing Commons 팜플렛"
-      on:click={handleDownloadClick}
-    />
+<div class='page'>
+  <div class='main'>
+    <div class='head'>
+      <div class='child logo'>
+        <img src='/images/SNUlogo.png' alt='snu logo' />
+      </div>
+      <div class='child title'>
+        <img src='/images/computingcommonslogo.png' alt='cc logo' />
+      </div>
+      <div class='child right'/>
     </div>
-  <button 
-    class='pamphlet_slide'
-    bind:this={divElement}
-    on:click={handleClick}
-    >
-    <PdfViewer
-      bind:this={pdfViewer}
-      props={{
-        path: pdfPath,
-        scale,
-        withAnnotations: true,
-        withTextContent: true,
-      }}
-      style="border: 1px solid black; display: block; height: 100%;"
-      on:page_changed={handlePageChanged}
-      on:load_failure={handleLoadFailure}
-      on:page_changed={handlePageChanged}
-    >
-    </PdfViewer>
-  </button>
-</div>
-<div class='contact'>
-  <input/>
-  <input />
-  <button />
+    <div class='body'>
+      <div class='child arrow left'>
+        {"<"}
+      </div>
+      <div class='child text'>
+        {"SNU COMPUTING COMMONS \n건축 기금을 모집합니다."}
+      </div>
+      <div class='child arrow right'>
+        {">"}
+      </div>
+    </div>
+    <div class="pbar">
+      <img src='/images/scroll.png' alt='scroll'/>
+    </div>
+  </div>
+  <div class='footer'>
+    <div class='blocks'>
+      <button />
+      <button />
+    </div>
+    <div class='description'>
+      <div class='child left' />
+      <div class='child snu'>
+
+      </div>
+      <div class='child contact'>
+
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
-  .title {
-    display: flex;
-  }
-  .logo {
-    flex: 4 1 0;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-  }
-  .logo h1 {
-    font-family: 'Current';
-    color: black;
-    src: url('/fonts/Current-Regular.otf');
-  }
-  .logo h2 {
-    font-family: 'Danjo';
-    color: black;
-    src: url('/fonts/Danjo-bold-Regular.otf');
-  }
-  .symbol {
-    flex: 1 1 0;
-  }
-  .symbol img {
-    width: 128px;
-    height: 128px;
-  }
-  .container {
+  .page {
     display: flex;
     width: 100%;
-    height: 128px;
-  }
-  .pamphlet_download {
-    display: flex;
-    flex: 1 1 0;
+    max-width: 1920px;
+    height: 1080px;
+    margin: 0;
+    padding: 0;
     justify-content: center;
-  }
-  .pamphlet_slide {
-    display: flex;
-    flex: 2 1 0;
-    justify-content: center;
-  }
-  .pdf_download {
-    height: 100%;
+    flex-direction: column;
+    background-image: URL("images/image1.png");
   }
 
-  .contact {
+  .main {
+    display: flex;
+    width: 100%;
+    height: 920px;
+    flex-direction: column;
+  }
+
+  .head {
+    display: flex;
+    height: 120px;
+    background: rgba(255, 255, 255, 0.5);
+  }
+  .head .child {
+    flex: 1;
+  }
+  .logo {
+    display: flex;
+    padding: 20px;
+    align-self: center;
+  }
+  .title {
     display: flex;
     justify-content: center;
+    align-items: center;
+  }
 
+  .body {
+    display: flex;
+    height: 760px;
+    flex: 1;
+  }
+
+  .body .child {
+    display: flex;
+  }
+
+  .arrow {
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 100px;
+    flex: 1;
+  }
+
+  .text {
+    flex: 10;
+    white-space: pre-wrap;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    font-family: 'Pretendard';
+    src: url("/fonts/Pretendard-Light.ttf");
+    font-size: 30px;
+    line-height: 2em;
+  }
+  .pbar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+  }
+  .footer {
+    height: 160px;
+    background-image: URL("images/lowerbar.png");
+  }
+  .blocks {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 120px;
+  }
+  .blocks > button {
+    width: 300px;
+    height: 50px;
+    margin: 50px;
+  }
+  .description {
+    height: 40px;
   }
 </style>
